0a8eb73f7fb38afdd7390335807cc9f76c19078a
Zachery-Beck <zacherybeck2020@gmail.com>
2024-03-16 02:15:35 -0400

    Small improvments

--- IndependentStudy.py
+++ IndependentStudy.py
@@ -1,9 +1,11 @@
+"""Generates df which incudes the TF-IDF"""
 import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import nltk
 
-def is_stopwords_downloaded():
+def is_stopwords_downloaded() -> None:
+    """Check to see if Stop words is downloaded if so return already dowladed else adds extra words list and stopwords list"""
     try:
         nltk.data.find('corpora/stopwords')
         print("Stopwords corpus is already downloaded.")
@@ -15,15 +17,16 @@ def is_stopwords_downloaded():
         nltk_stopwords = nltk.corpus.stopwords.words('english')
         nltk_stopwords.extend(custom_stopwords)
         print("Additional values have been added after downloading the stopwords corpus.")
+    
 
 
-def list_txt_files(directory):
+def list_txt_files(directory : str) -> list:
     """Return a list of full paths to .txt files in the specified directory."""
     all_files = os.listdir(directory)   
     txt_files = [os.path.join(directory, file) for file in all_files if file.endswith('.txt')]
     return txt_files
 
-def file_to_string(txt_files_list):
+def file_to_string(txt_files_list : list) -> str:
     """Read all content from a list of text files and concatenate into a single string."""
     all_content = ""
     for file in txt_files_list:
@@ -32,7 +35,7 @@ def file_to_string(txt_files_list):
             all_content += content + " " 
     return all_content
 
-def calculate_tf_idf_simplified(document_string):
+def calculate_tf_idf_simplified(document_string : str) -> pd.DataFrame:
     """Calculate TF-IDF scores for the words in the provided document string and return them as a DataFrame."""
     # Use NLTK's stop words
     nltk_stopwords = nltk.corpus.stopwords.words('english')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main File"""
 import os
 from dotenv import load_dotenv
 from IndependentStudy import list_txt_files, file_to_string, calculate_tf_idf_simplified, is_stopwords_downloaded
@@ -9,11 +10,7 @@ def __main__():
     txt_files_list = list_txt_files(input_directory)
     document_string = file_to_string(txt_files_list) 
     fixeddf = calculate_tf_idf_simplified(document_string)
-    
-    # Define the file path where you want to save the DataFrame
     output_file_path = 'output.csv'
-    
-    # Write the DataFrame to a CSV file
     fixeddf.to_csv(output_file_path, index=False)
     
     print(f"DataFrame has been written to {output_file_path}")
